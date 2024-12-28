fe9be040feaef518ea09fdb8ce08b07624af880b
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-28 16:55:25 -0700

    refresh.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl font-bold underline">
-        Hello World. Template is ready to go.
+        Hello World. Template is ready to go...
       </h1>
     </main>
   );
