2b251a249e9c08dd933f6659231145164be92430
Constantin <35843683+iFirebrand@users.noreply.github.com>
2024-12-28 17:20:58 -0700

    This gets the login working on nextjs and connected to a real vehicle in prod and gets a token back.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
+"use client";
+
+import {
+  LoginWithDimo,
+  initializeDimoSDK,
+  useDimoAuthState,
+  DimoAuthProvider,
+} from "@dimo-network/login-with-dimo";
+import { useState } from "react";
+
 export default function Home() {
+  const [permissionsEnabled] = useState(true);
+
+  // Initialize SDK first
+  initializeDimoSDK({
+    clientId: "removed",
+    redirectUri: "http://localhost",
+  });
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello World. Template is ready to go...
-      </h1>
-    </main>
+    <DimoAuthProvider>
+      <main className="flex min-h-screen items-center justify-center">
+        <LoginWithDimo
+          mode="popup"
+          onSuccess={(authData: {
+            jwt: string;
+            email?: string;
+            walletAddress?: string;
+          }) => console.log("Success:", authData)}
+          onError={(error) => console.error("Error:", error)}
+          permissionTemplateId={permissionsEnabled ? "1" : undefined}
+        />
+      </main>
+    </DimoAuthProvider>
   );
+}
+
+function AuthenticatedContent() {
+  const { isAuthenticated, getValidJWT, getEmail, email, walletAddress } =
+    useDimoAuthState();
+
+  return <div>{/* Use the auth state here */}</div>;
 }
